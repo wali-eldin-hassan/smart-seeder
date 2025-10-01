32a63c76f59e1a82aa3ee78483c42c7f26d2a54d
W.DAFALLA <W.DAFALLA@qu.edu.sa>
2025-10-01 11:12:44 +0300

    fix version issues

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit06ff3e039e9d8ce8f02544101b5e8a65
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit06ff3e039e9d8ce8f02544101b5e8a65', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit06ff3e039e9d8ce8f02544101b5e8a65', 'loadClassLoader'));
